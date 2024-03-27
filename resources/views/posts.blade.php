c97cbfff12da619aeac37bc3d41ce4d35461dfe3
Wahyu Safrizal <49548492+wahyusafrizal17@users.noreply.github.com>
2024-03-27 12:39:49 +0700

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
+<style>
+    p{
+        font-family: "Rockwell", serif;
+    }
+</style>
 
 <div class="row justify-content-center">
     <div class="col-md-3">
