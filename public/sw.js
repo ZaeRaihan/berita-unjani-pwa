84173e3a4c6c9682cd1e5eca683606351c9e2dad
GLAZERadr <adrianbadjideh@student.telkomuniversity.ac.id>
2024-04-05 21:17:39 +0700

    revisi sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,5 @@
-const preLoad = function () {
-    return caches.open("offline").then(function (cache) {
-        // caching index and important routes
-        return cache.addAll(filesToCache);
-    });
-};
-
-self.addEventListener("install", function (event) {
-    event.waitUntil(preLoad());
-});
+const staticCacheName =  'static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 
 const filesToCache = [
     "/",
@@ -36,45 +28,96 @@ const filesToCache = [
     "/offline.html",
 ];
 
-const checkResponse = function (request) {
-    return new Promise(function (fulfill, reject) {
-        fetch(request).then(function (response) {
-            if (response.status !== 404) {
-                fulfill(response);
-            } else {
-                reject();
-            }
-        }, reject);
-    });
-};
+// const preLoad = function () {
+//     return caches.open(staticCacheName).then(function (cache) {
+//         // caching index and important routes
+//         return cache.addAll(filesToCache);
+//     });
+// };
 
-const addToCache = function (request) {
-    return caches.open("offline").then(function (cache) {
-        return fetch(request).then(function (response) {
-            return cache.put(request, response);
+// self.addEventListener("install", function (event) {
+//     event.waitUntil(preLoad());
+// });
+
+// const checkResponse = function (request) {
+//     return new Promise(function (fulfill, reject) {
+//         fetch(request).then(function (response) {
+//             if (response.status !== 404) {
+//                 fulfill(response);
+//             } else {
+//                 reject();
+//             }
+//         }, reject);
+//     });
+// };
+
+// const addToCache = function (request) {
+//     return caches.open(staticCacheName).then(function (cache) {
+//         return fetch(request).then(function (response) {
+//             return cache.put(request, response);
+//         });
+//     });
+// };
+
+// const returnFromCache = function (request) {
+//     return caches.open(staticCacheName).then(function (cache) {
+//         return cache.match(request).then(function (matching) {
+//             if (!matching || matching.status === 404) {
+//                 return cache.match("offline.html");
+//             } else {
+//                 return matching;
+//             }
+//         });
+//     });
+// };
+
+// Kode Sebelumnya
+// self.addEventListener("fetch", function (event) {
+//     event.respondWith(
+//         checkResponse(event.request).catch(function () {
+//             return returnFromCache(event.request);
+//         })
+//     );
+//     if (!event.request.url.startsWith("http")) {
+//         event.waitUntil(addToCache(event.request));
+//     }
+// });
+
+//activate event
+self.addEventListener('install', evt => {
+    console.log('service worker installed');
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log('caching shell assets');
+        cache.addAll(filesToCache);
+      })
+    );
+  });
+
+// fetch event
+// untuk dynamic asset kayak di post yang butuh data DB
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+            })
         });
-    });
-};
-
-const returnFromCache = function (request) {
-    return caches.open("offline").then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            if (!matching || matching.status === 404) {
-                return cache.match("offline.html");
-            } else {
-                return matching;
-            }
-        });
-    });
-};
-
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        checkResponse(event.request).catch(function () {
-            return returnFromCache(event.request);
         })
     );
-    if (!event.request.url.startsWith("http")) {
-        event.waitUntil(addToCache(event.request));
-    }
+});
+
+// activate event
+// service worker akan membersihkan cache-cache yang tidak lagi digunakan setelah versi service worker yang baru diinstal aktif untuk static dan dynamic asset 
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+      caches.keys().then(keys => {
+        return Promise.all(keys
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+          .map(key => caches.delete(key))
+        );
+      })
+    );
 });
